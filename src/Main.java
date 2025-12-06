7ee18576a3717cf6c34eec60c6143b08d9347824
Zadkiel <zadkielbd@outlook.com>
2025-12-05 20:09:02 -0600

    HandelAssessv0.5

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+//Codigos ANSI color del texto
 public static final String RESET = "\u001B[0m";
 public static final String ROJO = "\u001B[31m";
 public static final String VERDE = "\u001B[32m";
@@ -5,78 +6,119 @@ public static final String AMARILLO = "\u001B[33m";
 public static final String AZUL = "\u001B[34m";
 public static final String MORADO = "\u001B[35m";
 public static final String CIAN = "\u001B[36m";
-public static final String BLANCO = "\u001B[37m";
+public static final String NEGRO = "\u001B[30m";
+
+//Codigos ANSI color del fondo
+public static final String NEGRO_FONDO = "\u001B[40m";
+public static final String ROJO_FONDO = "\u001B[41m";
+public static final String VERDE_FONDO = "\u001B[42m";
+public static final String AMARILLO_FONDO = "\u001B[43m";
+public static final String AZUL_FONDO = "\u001B[44m";
+public static final String MORADO_FONDO = "\u001B[45m";
+public static final String CIAN_FONDO = "\u001B[46m";
+public static final String BLANCO_FONDO = "\u001B[47m";
+
 
 void main() {
     int opcInicio = 0; //opcion para el inicio de sesion o crear cuenta
     int opcTipo; // opcion para el tipo de cuenta
     String opcionIngresada; // sirve para obtener la opcion ingresada en un string y asignarlo a una opcion tambien para validar en el try catch
     String tipo = ""; // Inicializar tipo
-    String nombre;  // Inicializar nombre
-    String contrasena; // Inicializar contraseña
-    boolean correcto; // Inicializar correcto
+    String nombre; // Inicializar nombre
+    String contraseña; // Inicializar contraseña
+    boolean correcto = false; // Inicializar correcto
     boolean acceso = false; // Inicializar acceso
+    String contraAdmin = "HANDEL_ADMIN123";
 
-    do { //Bucle para el modulo de login
+    Usuario.verificarArchivo();
+
+    while (!acceso) { //Bucle para el modulo de login
         do { //Bucle para ingresar si se quiere Iniciar sesion o crear una cuenta
-            IO.println(MORADO+"Iniciar sesión o crear cuenta"+RESET);
-            IO.println("1)Crear cuenta");
-            IO.println("2)Iniciar sesión");
-            opcionIngresada = IO.readln();
+            IO.println(MORADO+"=====Iniciar sesión o crear cuenta====="+RESET);
+            IO.println(VERDE+"1)Crear cuenta\n2)Iniciar sesión"+RESET);
+            opcionIngresada = IO.readln().trim();
             //try catch para validar si la opcion ingresada si sea un numero
             try {
                 opcInicio = Integer.parseInt(opcionIngresada); //convirtiendo el string(opcionIngresada) a numero y asignandoselo a opcInicio
-                if (opcInicio <= 0 || opcInicio > 2) { //validando si la opcion es valida
-                    correcto = false;
-                    IO.println(ROJO + "Debes de ingresar 1 o 2" + RESET);
-                } else {
-                    correcto = true;
+                switch (opcInicio) {
+                    case 1:
+                        IO.println(AZUL+"=====Creando Cuenta====="+RESET);
+                        correcto = true;
+                        break;
+                    case 2:
+                        IO.println(AZUL+"=====Iniciando Sesion====="+RESET);
+                        correcto = true;
+                        break;
+                    default:
+                        IO.println(ROJO+"Debes de ingresar 1 o 2"+RESET);
+                        break;
                 }
             } catch (NumberFormatException e) { //atrapa la excepcion
-                IO.println(ROJO + "Debes de ingresar un numero" + RESET);
-                correcto = false;
+                IO.println(ROJO+"Debes de ingresar un numero"+RESET);
             }
-        } while (!correcto);
+        } while(!correcto);
         do { //Bucle para el ingreso de los datos
-            nombre = IO.readln("Nombre: ").trim().toLowerCase();
-            contrasena = IO.readln("Contraseña: ").trim();
-            IO.println(MORADO + "Tipos de cuentas:" + RESET);
-            IO.println("1)Usuario");
-            IO.println("2)Administrador");
-            opcionIngresada = IO.readln();
+            IO.println(MORADO+"Tipos de cuentas:"+RESET);
+            IO.println(VERDE+"1)Usuario\n2)Administrador"+RESET);
+            opcionIngresada = IO.readln().trim();
             //try catch para validar si la opcion ingresada si sea un numero
             try {
                 opcTipo = Integer.parseInt(opcionIngresada); // convirtiendo a numero opcionIngresada y asignandole a opcTipo
-                if (opcTipo == 1) {
-                    tipo = "Usuario";
-                    correcto = true;
-                } else if (opcTipo == 2) {
-                    tipo = "Administrador";
-                    correcto = true;
-                } else {
-                    tipo = "Error";
-                    IO.println(ROJO + "Opcion Invalida" + RESET);
-                    correcto=false;
+                switch (opcTipo) {
+                    case 1:
+                        tipo = "Usuario";
+                        correcto = true;
+                        break;
+                    case 2:
+                        if (opcInicio==1) {
+                            String contraAdminIngresado = IO.readln("Ingresa la contraseña del administrador: ");
+                            if (contraAdminIngresado.equalsIgnoreCase(contraAdmin)) {
+                                tipo = "Administrador";
+                                correcto = true;
+                            } else {
+                                IO.println("Contraseña del administrador incorrecto");
+                                correcto = false;
+                            }
+                        } else if (opcInicio==2) {
+                            tipo = "Administrador";
+                            correcto = true;
+                        }
+                        break;
+                    default:
+                        tipo = "Error";
+                        IO.println(ROJO+"Opcion Invalida"+RESET);
+                        correcto=false;
+                        break;
                 }
-                if (nombre.isEmpty() || contrasena.isEmpty() || tipo.isEmpty()) { //validando si algun valor esta vacio
-                    correcto = false;
-                    IO.println(ROJO + "Ningun campo puede ser nulo" + RESET);
-                }
-            } catch (NumberFormatException e) { //atrapa la excepcion
-                IO.println(ROJO + "Debes de ingresar un numero" + RESET);
+            }
+            catch (NumberFormatException e) { //atrapa la excepcion
+                IO.println(ROJO+"Debes de ingresar un numero"+RESET);
+                correcto = false;
+            }
+        } while (!correcto);
+        do {
+            nombre = IO.readln("Nombre: ").trim();
+            contraseña = IO.readln("Contraseña: ").trim();
+            if (nombre.isEmpty() || contraseña.isEmpty()) { //validando si algun valor esta vacio
+                correcto = false;
+                IO.println(ROJO+"Ningun campo puede ser nulo"+RESET);
             }
         } while (!correcto);
         if (opcInicio == 1) { // Si la opcion es 1 crea una cuenta
-            Usuario.crearCuenta(nombre, contrasena, tipo); //Insertar los valores de nombre contraseña, tipo al modulo crear cuenta
-        } else { // Si la opcion es 2 inicia sesion
+            boolean verificarExistente = Usuario.verificarExistente(nombre, contraseña, tipo);
+            if (!verificarExistente) {
+                Usuario.crearCuenta(nombre, contraseña, tipo); //Insertar los valores de nombre contraseña, tipo al modulo crear cuenta
+            }
+        } else if (opcInicio == 2) { // Si la opcion es 2 inicia sesion
             Usuario.cargarUsuarios(); //llama al metodo cargar usuarios
-            boolean inicioSesion = Usuario.iniciarSesion(nombre, contrasena, tipo); //Insertar los valores de nombre contraseña, tipo al modulo iniciar sesion
+            boolean inicioSesion = Usuario.iniciarSesion(nombre, contraseña, tipo); //Insertar los valores de nombre contraseña, tipo al modulo iniciar sesion
             if (inicioSesion) { // verifica si el metodo iniciar sesion es verdadero
-                System.out.printf("hola %s, has accedido como %s\n",nombre, tipo);
+                System.out.printf(AZUL_FONDO+" Hola %s, has accedido como %s "+RESET+RESET,nombre, tipo);
                 acceso = true;
             }
         }
-    } while (!acceso);
+    }
 }
+
 
 
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Usuario {
@@ -13,18 +10,19 @@ public class Usuario {
     public static final String MORADO = "\u001B[35m";
     public static final String CIAN = "\u001B[36m";
     public static final String BLANCO = "\u001B[37m";
-    private static final String FILE = "Usuarios.txt";
+    private static final String FILE = "./Usuarios.txt";
+    private static final ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();//se crea el arrayList
+    private static final File file = new File(FILE);
 
-    private static ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();//se crea el arrayList
     private String Nombre;
-    private String contrasena;
+    private String contraseña;
     private String tipo;
 
     //Metodo de usuario
 
-    public Usuario(String nombre, String contrasena, String tipo) {
+    public Usuario(String nombre, String contraseña, String tipo) {
         this.tipo = tipo;
-        this.contrasena = contrasena;
+        this.contraseña = contraseña;
         this.Nombre = nombre;
     }
 
@@ -32,12 +30,10 @@ public class Usuario {
     public String getNombre() {
         return Nombre;
     }
-
-    public String getContrasena() {
-        return contrasena;
+    public String getContraseña() {
+        return contraseña;
     }
-
-    public String isTipo() {
+    public String getTipo() {
         return tipo;
     }
 
@@ -45,13 +41,24 @@ public class Usuario {
     public void setNombre(String nombre) {
         Nombre = nombre;
     }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
     }
-
     public void setTipo(String tipo) {
         this.tipo = tipo;
+    }
+
+    public static void verificarArchivo() {
+        if (!file.exists()) {
+            IO.println(ROJO+"Archivo no encontado"+RESET);
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE))) {
+                bw.write(String.format("|%-17s %s %17s| %n","","=====LISTA USUARIOS=====",""));
+                bw.write(String.format("|%-20s |%-20s |%-15s |%n", "NOMBRE", "CONTRASEÑA", "TIPO"));
+                IO.println("Se ha creado el archivo: " + file.getName());
+            } catch (IOException e) {
+                System.out.println("Ocurrio un error");
+            }
+        }
     }
 
     public static void cargarUsuarios() {
@@ -60,29 +67,32 @@ public class Usuario {
             BufferedReader br = new BufferedReader(new FileReader(FILE));
             String linea;
             while ((linea = br.readLine()) != null) {
-                String[] partes = linea.split(",");
+                linea = linea.replaceFirst("^\\|", "").replaceFirst("\\|$", "");
+                String[] partes = linea.split("\\|");
                 if (partes.length == 3) {
-                    Usuario u = new Usuario(partes[0], partes[1], partes[2]);
-                    listaUsuarios.add(u); // usuario[0] = nombre, usuario[1] = contraseña
+                    listaUsuarios.add(new Usuario(partes[0].trim(),partes[1].trim(),partes[2].trim()));
+                     // parte[0] = nombre, parte[1] = contraseña, parte[2] = tipo
                 }
             }
         } catch (IOException e) {
-            IO.println(ROJO+"Archivo no encontrado, Se creara uno"+RESET);
+            IO.println("Errar al leer usuarios: "+e.getMessage());
         }
     }
 
-    static void crearCuenta(String nombre, String contrasena, String tipo) {
-        try(FileWriter fw = new FileWriter(FILE, true)) {
-            fw.write(nombre + "," + contrasena + ","+ tipo + "\n");
+    static void crearCuenta(String nombre, String contraseña, String tipo) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(FILE, true))) {
+            bw.write(String.format( "|%-20s |%-20s |%-15s |%n",nombre,contraseña,tipo));
             IO.println(AZUL+"****Cuenta creada correctamente****"+RESET);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    static boolean iniciarSesion(String nombre, String contrasena, String tipo) {
-        for (Usuario i: listaUsuarios) {
-            if (i.getNombre().equals(nombre)&&i.getContrasena().equals(contrasena)&&i.isTipo().equals(tipo)){
+    static boolean iniciarSesion(String nombre, String contraseña, String tipo) {
+        for (Usuario usuario: listaUsuarios) {
+            if (usuario.getNombre().equalsIgnoreCase(nombre)&&
+                    usuario.getContraseña().equals(contraseña)&&
+                    usuario.getTipo().equals(tipo)){
                 IO.println(AZUL+"****Iniciando sesion****"+RESET);
                 return true;
             }
@@ -90,4 +100,17 @@ public class Usuario {
         IO.println(ROJO+"Usuario no encontrado, vuelve a intentarlo"+RESET);
         return false;
     }
+
+    static boolean verificarExistente(String nombre, String contraseña, String tipo) {
+        Usuario.cargarUsuarios();
+        for (Usuario usuario: listaUsuarios) {
+            if (usuario.getNombre().equalsIgnoreCase(nombre)&&
+                    usuario.getContraseña().equals(contraseña)&&
+                    usuario.getTipo().equals(tipo)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
